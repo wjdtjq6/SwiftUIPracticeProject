7c7e4e67304de13231b44b659f4f66e44c8f868e
jimmy <wjdtjq6@gmail.com>
2024-09-02 14:15:03 +0900

    Initial Commit

---
+++ SwiftUIPracticeProject/SwiftUIPracticeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeProjectApp.swift
+//  SwiftUIPracticeProject
+//
+//  Created by 소정섭 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
